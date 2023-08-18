a903429c649524797e4ce8e77ac48b3772d0fa17
Nizor <ogbezuodenizor@yahoo.com>
2023-08-18 14:26:58 +0100

    deleted views and template

--- views/views.py
+++
@@ -1,8 +0,0 @@
-import json
-from django.shortcuts import render
-
-def json_data(request):
-    with open('data.json') as json_file:
-        data = json.load(json_file)
-    
-    return render(request, 'json_template.html', {'data': data})
